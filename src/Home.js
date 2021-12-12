fefae74de9a1891eab51ef76cebedcc4320df719
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-11 19:40:42 -0500

    Spike success done

---
+++ src/Home.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { data } from "./data";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
+
+export const Home = () => {
+
+    return (
+        <section className='container'>
+            <div className="row justify-center">
+                <table className='table table-striped' id="icbf-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Edad</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {data.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.nombre}</td>
+                                <td>{item.edad}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <div className='col-12'>
+                    <ReactHTMLTableToExcel 
+                        sheet="Sheet"
+                        table="icbf-table" 
+                        filename="ICBF Excel file"
+                        buttonText="Export to Excel"
+                        className="btn btn-info mx-auto d-block" />
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: 1,
+        edad: 1,
+        nombre: "Laura Ipuana"
+    },
+    {
+        id: 2,
+        edad: 2,
+        nombre: "Jose Pushaina"
+    },
+    {
+        id: 3,
+        edad: 1,
+        nombre: "Pedro Uriana"
+    }
+];
